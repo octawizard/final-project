737dc925bbffedd0af33188b7777f373a42341ce
octawizard <roberto.manca.89@gmail.com>
2013-06-17 22:05:08 +0200

    update LC16

--- LC16.js
+++ LC16.js
@@ -1,7 +1,7 @@
 //LC 16 , e LC14 tabourets, in legno
 wood = [166.0/255, 128.0/255, 100.0/255];
 
-//libreria, modello base
+//libreria, modello base, parametri
 var height = 7.2;
 
 var gr_base_x = 6.93;
@@ -22,53 +22,43 @@ var int_base1_x = 6.6;
 var int_base1_y = 2.6;
 var int_base1_z = 0.2;
 
-var ground_base = CUBOID([gr_base_x, gr_base_y, gr_base_z]);
-ground_base = T([0])([-gr_base_x/2])(ground_base);
-var library_base = CUBOID([lib_base_x, lib_base_y, lib_base_z]);
-library_base = T([0,1,2])([-lib_base_x/2, -(lib_base_y-gr_base_y)/2, gr_base_z])(library_base);
+function genericLibrary(){
+	var ground_base = CUBOID([gr_base_x, gr_base_y, gr_base_z]);
+	ground_base = T([0])([-gr_base_x/2])(ground_base);
+	var library_base = CUBOID([lib_base_x, lib_base_y, lib_base_z]);
+	library_base = T([0,1,2])([-lib_base_x/2, -(lib_base_y-gr_base_y)/2, gr_base_z])(library_base);
+	var library_top = T([2])([gr_base_z + lib_base_z + dist_base_int1 + dist_int1_int2 + dist_int2_top])(library_base);
+	var library_back = CUBOID([lib_base_x, 0.2, height - gr_base_z]);
+	library_back = T([0,1,2])([-lib_base_x/2, 2.7 - 0.3, gr_base_z])(library_back);
+	var library_dx =  CUBOID([0.2, 2.7, 7]);
+	library_dx= T([0,1,2])([int_base1_x/2 , -0.1, gr_base_z])(library_dx);
+	var library_sx = T([0])([-2*int_base1_x/2 - gr_base_z])(library_dx);
+	return STRUCT([ground_base, library_base, library_top, library_back, library_dx, library_sx]);
+}
 
-var intermediate_base_1 = CUBOID([int_base1_x, int_base1_y, int_base1_z]);
-intermediate_base_1 = T([0,1,2])([-int_base1_x/2, -(lib_base_y-int_base1_y) , dist_base_int1 + gr_base_z + lib_base_z])(intermediate_base_1);
+function sideLibrary(){
+	var intermediate_base_1 = CUBOID([int_base1_x, int_base1_y, int_base1_z]);
+	intermediate_base_1 = T([0,1,2])([-int_base1_x/2, -(lib_base_y-int_base1_y) , dist_base_int1 + gr_base_z + lib_base_z])(intermediate_base_1);
+	var intermediate_base_2 = T([2])([dist_int1_int2])(intermediate_base_1);
+	return STRUCT([intermediate_base_1, intermediate_base_2, genericLibrary()]);
+}
 
-var intermediate_base_2 = T([2])([dist_int1_int2])(intermediate_base_1);
+function centralLibrary(){
+	var library_int_cx = CUBOID([int_base1_x, int_base1_y, int_base1_z]);
+	library_int_cx = T([0,1,2])([-int_base1_x/2, -(lib_base_y-int_base1_y) , dist_base_int1 + gr_base_z + lib_base_z +1.2])(library_int_cx);
+	return STRUCT([genericLibrary(), library_int_cx]);
+}
+//composizione della libreria
+function getLibrary(){
+	var lib_sx = T([0])([-lib_base_x])(sideLibrary());
+	var lib_dx = T([0])([lib_base_x])(sideLibrary());
+	var lib_cx = centralLibrary();
+	var lib = STRUCT([lib_sx, lib_dx, lib_cx]);
+	return lib;
+}
 
-var library_top = T([2])([gr_base_z + lib_base_z + dist_base_int1 + dist_int1_int2 + dist_int2_top])(library_base);
 
-var library_back = CUBOID([lib_base_x, 0.2, height - gr_base_z]);
-library_back = T([0,1,2])([-lib_base_x/2, 2.7 - 0.3, gr_base_z])(library_back);
-
-var library_dx =  CUBOID([0.2, 2.7, 7]);
-library_dx= T([0,1,2])([int_base1_x/2 , -0.1, gr_base_z])(library_dx);
-var library_sx = T([0])([-2*int_base1_x/2 - gr_base_z])(library_dx);
-
-var library = STRUCT([ground_base, library_base, intermediate_base_1, intermediate_base_2, library_top, library_back, library_dx, library_sx]);
-
-
-//tavolino collegato alla libreria
-//pilastro
-var cil_base = DISK(0.55)([32,4]);
-var cil_surface = CYL_SURFACE([0.55, 7.15])([32,4]);
-var pillar = STRUCT([cil_surface, cil_base]);
-pillar = T([0,1])([1, -7 +1.2])(pillar);
-
-//tavolino
-var top_table = CUBOID([5.3, 7.5, 0.2]);
-var btx_1 =  CUBOID([5.3, 0.2, 0.7]);
-btx_1 = T([2])([-0.7])(btx_1);
-var btx_2 = T([1])([7.5])(btx_1);
-var bty_1 = CUBOID([0.2, 7.5, 0.7]);
-bty_1 = T([2])([-0.7])(bty_1);
-var bty_2 = T([0])([5.3 -0.2])(bty_1);
-
-var plane = STRUCT([top_table, btx_1, btx_2, bty_1, bty_2]);
-plane = T([0,1,2])([-(7-5.3) -0.1, -7, 7.04])(plane);
-
-//scatole "sedia"
-
-//devo definire i punti della scatola
-//altezza = 4
-//y= 4
-//x= 3
+//SEDIE "SCATOLE" LC14 		""""" altezza = 4 ; y= 4 ; x= 3 """""
 var domainPI = DOMAIN([[0,1],[0,2*PI]])([20,50]);
 var domain1D = INTERVALS(1)(20);
 var domain2D = DOMAIN([[0,1],[0,1]])([40,40]);
@@ -101,9 +91,6 @@ function getSurfaceZ() {
 	return surface_z;
 }
 
-var surface_z = (getSurfaceZ());
-var surface_z_top = T([2])([4])(surface_z);
-
 //facciate perpendicolare al pavimento - 1
 function getSurfaceX() {
 	var profile_edge_1 = BEZIER(S0)([[-2, -2, 0], [-2, 2, 0]]);
@@ -132,12 +119,6 @@ function getSurfaceX() {
 	return surface_x;
 }
 
-var surface_x = (getSurfaceX());
-surface_x = R([0,1])([PI/2])(surface_x);
-surface_x = R([0,2])([PI/2])(surface_x);
-surface_x = T([0,1,2])([1.5,0,2])(surface_x);
-var surface_x_2 = T([0])([-3])(surface_x);
-
 //facciate perpendicolare al pavimento - 2
 function getSurfaceY() {
 	var profile_edge_1 = BEZIER(S0)([[-1.5, -2, 0], [-1.5, 2, 0]]);
@@ -149,29 +130,55 @@ function getSurfaceY() {
 	return MAP(profile_surface_y1)(domain2D);
 }
 
-var surface_y = getSurfaceY();
-surface_y = R([1,2])([PI/2])(surface_y);
-surface_y = T([0,1,2])([0,2,2])(surface_y);
-var surface_y_2 = T([1])([-4])(surface_y);
+function getChair(){
+	var surface_z = (getSurfaceZ());
+	var surface_z_top = T([2])([4])(surface_z);
+	var surface_x = (getSurfaceX());
+	surface_x = R([0,1])([PI/2])(surface_x);
+	surface_x = R([0,2])([PI/2])(surface_x);
+	surface_x = T([0,1,2])([1.5,0,2])(surface_x);
+	var surface_x_2 = T([0])([-3])(surface_x);
+	var surface_y = getSurfaceY();
+	surface_y = R([1,2])([PI/2])(surface_y);
+	surface_y = T([0,1,2])([0,2,2])(surface_y);
+	var surface_y_2 = T([1])([-4])(surface_y);
+	var chair = STRUCT([surface_z, surface_z_top, surface_x, surface_x_2, surface_y, surface_y_2]);
+	return chair;
+}
 
+function getChairs(){
+	var chair1 = T([0,1])([-4, -3.5])(getChair());
+	var chair2 =  T([0])([8])(chair1);
+	return STRUCT([chair1, chair2]);
+}
 
+//TAVOLINO COLLEGATO ALLA LIBRERIA
+function getTable(){
+	var pillar = T([0,1])([1, -7 +1.2])(getPillar(0.55, 7.15, [32,4]));
+	var plane = T([0,1,2])([-(7-5.3) -0.1, -7, 7.04])(getPlane());
+	return STRUCT([pillar, plane]);
+}
 
-var chair1 = STRUCT([surface_z, surface_z_top, (surface_x), surface_x_2, surface_y, surface_y_2]);
-chair1 = T([0,1])([-4, -3.5])(chair1)
-var chair2 =  T([0])([8])(chair1)
-var chairs = STRUCT([chair1, chair2]);
+function getPillar(r, h, dim){
+	var cil_base = DISK(r)(dim);
+	var cil_surface = CYL_SURFACE([r, h])(dim);
+	var pillar = STRUCT([cil_surface, cil_base]);
+	return pillar;
+}
 
-//composizione della libreria, tavolo e sedie
-var lib_sx = T([0])([-lib_base_x])(library);
-var lib_dx = T([0])([lib_base_x])(library);
-var library_int_cx = T([2])([1.2])(intermediate_base_1);
-var lib_cx = STRUCT([ground_base, library_base, library_top, library_back, library_dx, library_sx, library_int_cx]);
-var lib = STRUCT([lib_sx, lib_dx, lib_cx]);
+function getPlane(){
+	var top_table = CUBOID([5.3, 7.5, 0.2]);
+	var btx_1 =  CUBOID([5.3, 0.2, 0.7]);
+	btx_1 = T([2])([-0.7])(btx_1);
+	var btx_2 = T([1])([7.5])(btx_1);
+	var bty_1 = CUBOID([0.2, 7.5, 0.7]);
+	bty_1 = T([2])([-0.7])(bty_1);
+	var bty_2 = T([0])([5.3 -0.2])(bty_1);
+	var plane = STRUCT([top_table, btx_1, btx_2, bty_1, bty_2]);
+	return plane;
+}
 
-var table = STRUCT([pillar, plane]);
-
-var modelLC16_LC14 = STRUCT([lib, table, chairs]);
+// MODELLI LC16 E LC14
+var modelLC16_LC14 = STRUCT([getLibrary(), getTable(), getChairs()]);
 modelLC16_LC14 = COLOR(wood)(modelLC16_LC14);
 DRAW(modelLC16_LC14)
-
-//FARE SPESSORE DELLE SEDIE LC14
